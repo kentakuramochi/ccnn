1cb64e4f41a2c7aafa5364f44c21e58d8098b41c
kentakuramochi <soratobi93@gmail.com>
2020-02-28 00:41:30 +0900

    renew basic layer structure

---
+++ include/layer.h
@@ -0,0 +1,77 @@
+///
+/// @file   layer.h
+/// @brief  network layer
+///
+
+#ifndef LAYER_H
+#define LAYER_H
+
+#include <stdlib.h>
+
+#include "mat.h"
+
+///
+/// @struct layer_t
+/// @brief  layer structure
+///
+typedef struct layer_t {
+    mat_t* x;   ///< input
+    mat_t* y;   ///< output
+    mat_t* w;   ///< weight
+    mat_t* b;   ///< bias
+
+    struct layer_t* prev;   ///< pointer of prev layer
+    struct layer_t* next;   ///< pointer of next layer
+
+    void (*forward)(struct layer_t*, const mat_t*);   ///< forward processing
+    void (*backward)(struct layer_t*, const mat_t*);  ///< backward processing
+} layer_t;
+
+///
+/// @fn     layer_alloc
+/// @brief  allocate new layer
+/// @return pointer to new layer
+///
+layer_t* layer_alloc(void)
+{
+    layer_t* layer = (layer_t*)malloc(sizeof(layer_t));
+    if (layer == NULL) {
+        return NULL;
+    }
+
+    layer->x = NULL;
+    layer->y = NULL;
+
+    layer->w = NULL;
+    layer->b = NULL;
+
+    layer->prev = NULL;
+    layer->next = NULL;
+
+    layer->forward  = NULL;
+    layer->backward = NULL;
+
+    return layer;
+}
+
+///
+/// @fn     layer_free
+/// @brief  free layer
+/// @param[in]  layer   layer
+///
+void layer_free(layer_t* layer)
+{
+    if (layer == NULL) {
+        return;
+    }
+
+    mat_free(layer->x);
+    mat_free(layer->y);
+
+    mat_free(layer->w);
+    mat_free(layer->b);
+
+    free(layer);
+}
+
+#endif
--- test/test_layer.c
+++ test/test_layer.c
@@ -1,96 +1,43 @@
 ///
-/// @file   test_layer.cpp
-/// @brief  test of base layer
+/// @file   test_layer.c
+/// @brief  test of basic layer
 ///
 
 #include "unity.h"
 
-#include "layer/layer.h"
+#include "layer.h"
 
-void setUp(void) {}
-
-void tearDown(void) {}
-
-void test_create(void)
+void setUp(void)
 {
-    layer_t* layer = (layer_t*)layer_create();
+}
 
-    TEST_ASSERT_EQUAL(LAYER_NONE, layer->type);
-    TEST_ASSERT_NULL(layer->in);
-    TEST_ASSERT_NULL(layer->out);
+void tearDown(void)
+{
+}
+
+void test_layer_alloc(void)
+{
+    layer_t* layer = layer_alloc();
+
+    TEST_ASSERT_NOT_NULL(layer);
+
+    TEST_ASSERT_NULL(layer->x);
+    TEST_ASSERT_NULL(layer->y);
     TEST_ASSERT_NULL(layer->w);
     TEST_ASSERT_NULL(layer->b);
     TEST_ASSERT_NULL(layer->prev);
     TEST_ASSERT_NULL(layer->next);
     TEST_ASSERT_NULL(layer->forward);
+    TEST_ASSERT_NULL(layer->backward);
 
-    layer_delete(layer);
-}
-
-void test_create_data(void)
-{
-    layer_t* layer = (layer_t*)layer_create();
-
-    layer_create_input(layer, 1, 3, 480, 640);
-    TEST_ASSERT_EQUAL(1, layer->in->n);
-    TEST_ASSERT_EQUAL(3, layer->in->c);
-    TEST_ASSERT_EQUAL(480, layer->in->h);
-    TEST_ASSERT_EQUAL(640, layer->in->w);
-    TEST_ASSERT_EQUAL((1 * 3 * 480 * 640), layer->in->elem);
-
-    layer_create_output(layer, 1, 10, 240, 320);
-    TEST_ASSERT_EQUAL(1, layer->out->n);
-    TEST_ASSERT_EQUAL(10, layer->out->c);
-    TEST_ASSERT_EQUAL(240, layer->out->h);
-    TEST_ASSERT_EQUAL(320, layer->out->w);
-    TEST_ASSERT_EQUAL((1 * 10 * 240 * 320), layer->out->elem);
-
-    layer_create_weight(layer, 10, 3, 5, 5);
-    TEST_ASSERT_EQUAL(10, layer->w->n);
-    TEST_ASSERT_EQUAL(3, layer->w->c);
-    TEST_ASSERT_EQUAL(5, layer->w->h);
-    TEST_ASSERT_EQUAL(5, layer->w->w);
-    TEST_ASSERT_EQUAL((10 * 3 * 5 * 5), layer->w->elem);
-
-    layer_create_bias(layer, 1, 10, 1, 1);
-    TEST_ASSERT_EQUAL(1, layer->b->n);
-    TEST_ASSERT_EQUAL(10, layer->b->c);
-    TEST_ASSERT_EQUAL(1, layer->b->h);
-    TEST_ASSERT_EQUAL(1, layer->b->w);
-    TEST_ASSERT_EQUAL((1 * 10 * 1 * 1), layer->b->elem);
-
-    layer_delete(layer);
-}
-
-void test_connect(void)
-{
-    layer_t* layer1 = (layer_t*)layer_create();
-    layer_create_output(layer1, 1, 3, 32, 32);
-
-    layer_t* layer2 = (layer_t*)layer_create();
-    layer_connect(layer1, layer2);
-
-    TEST_ASSERT_EQUAL(1, layer2->in->n);
-    TEST_ASSERT_EQUAL(3, layer2->in->c);
-    TEST_ASSERT_EQUAL(32, layer2->in->h);
-    TEST_ASSERT_EQUAL(32, layer2->in->w);
-
-    TEST_ASSERT_NULL(layer1->prev);
-    TEST_ASSERT_EQUAL(layer2, layer1->next);
-    TEST_ASSERT_EQUAL(layer1, layer2->prev);
-    TEST_ASSERT_NULL(layer2->next);
-
-    layer_delete(layer1);
-    layer_delete(layer2);
+    layer_free(layer);
 }
 
 int main(void)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_create);
-    RUN_TEST(test_create_data);
-    RUN_TEST(test_connect);
+    RUN_TEST(test_layer_alloc);
 
     return UNITY_END();
 }
