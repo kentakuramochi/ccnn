20cb8281873d84fed4b6e038200498b96d6aef9a
kentakuramochi <soratobi93@gmail.com>
2020-03-21 23:59:21 +0900

    add func comment

--- include/fc_layer.h
+++ include/fc_layer.h
@@ -8,12 +8,24 @@
 
 #include "layer.h"
 
+///
+/// @fn     fc_forward
+/// @brief  forward 
+/// @param[in]  fc  fc layer
+/// @param[in]  x   input matrix
+///
 static void fc_forward(layer_t* fc, const mat_t* x)
 {
     mat_mul(x, fc->w, fc->y);
     mat_add(fc->y, fc->b, fc->y);
 }
 
+///
+/// @fn     fc
+/// @brief  alloc fc layer
+/// @param[in]  x   input width 
+/// @param[in]  y   output width
+///
 layer_t* fc(const int x, const int y)
 {
     layer_t* fc = layer_alloc();
