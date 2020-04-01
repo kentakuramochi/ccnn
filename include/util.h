536d8986e06d25e2b1a42799b7a30d46a6c2b645
kentakuramochi <soratobi93@gmail.com>
2020-04-01 23:59:20 +0900

    add func of random seed

--- include/util.h
+++ include/util.h
@@ -14,18 +14,27 @@ static uint32_t xorsft_z = 521288629;
 static uint32_t xorsft_w = 88675123;
 
 ///
-/// @fn     xorshift
-/// @brief  xorshift algorithm
-/// @return pseudo random value
+/// @fn     random_seed
+/// @brief  set seed of random number
 ///
-uint32_t xorshift(void)
+void random_seed(uint32_t seed)
+{
+    xorsft_w = seed;
+}
+
+///
+/// @fn     xorshift_128
+/// @brief  generate random number by xorshift algorithm
+/// @return pseudo random number
+///
+uint32_t xorshift_128(void)
 {
     uint32_t t = xorsft_x ^ (xorsft_x << 11);
 
     xorsft_x = xorsft_y;
     xorsft_y = xorsft_z;
     xorsft_z = xorsft_w;
-    xorsft_w ^= t ^ (t >> 8) ^ (xorsft_w >> 19);
+    xorsft_w ^= (t ^ (t >> 8)) ^ (xorsft_w >> 19);
 
     return xorsft_w;
 }
