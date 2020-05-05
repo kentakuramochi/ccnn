6a4343955d8a2317ad2b9c61be5c64a3de5dfe99
kentakuramochi <soratobi93@gmail.com>
2020-05-05 23:58:57 +0900

    add rand test

--- test/test_util.c
+++ test/test_util.c
@@ -25,11 +25,33 @@ void test_rand_uint(void)
     TEST_ASSERT_NOT_EQUAL(x1, x2);
 }
 
+void test_rand_uniform(void)
+{
+    rand_seed(0);
+
+    float x1 = rand_uniform();
+    float x2 = rand_uniform();
+
+    TEST_ASSERT_NOT_EQUAL(x1, x2);
+}
+
+void test_rand_normal(void)
+{
+    rand_seed(0);
+
+    float x1 = rand_normal(0, 0.1);
+    float x2 = rand_normal(0, 0.1);
+
+    TEST_ASSERT_NOT_EQUAL(x1, x2);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_rand_uint);
+    RUN_TEST(test_rand_uniform);
+    RUN_TEST(test_rand_normal);
 
     return UNITY_END();
 }
